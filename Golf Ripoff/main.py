483cd14067cb1c208adce92cee9171af8505bc05
SaggyTroy <josephmei0311@gmail.com>
2024-08-25 14:42:58 -0400

    update with Foo-Food and Golf

---
+++ Golf Ripoff/main.py
@@ -0,0 +1,20 @@
+import vpython
+
+scene.width = 600
+scene.height = 900
+scene.userzoom = False
+scene.userspin = False
+scene.userpan = False
+scene.camera.pos = vector(0,0,50)
+#scene.range += 100
+
+
+
+
+stat_fric = 0
+
+kin_fric = 0
+
+gravity = 9.8
+
+sphere(pos=vector(0,0,0))
